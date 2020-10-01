283a43236532b7fd18146a9420452922359f3eef
Hannah Pacheco <hannah.pacheco@daviestech.org>
2020-10-01 15:52:42 +0000

    fixed a spelling error

--- Question3.java
+++ Question3.java
@@ -1,16 +1,17 @@
 
-
+import java.until.Scanner;
 class Question3 {
     public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
     String name;
 
-    System.out.println(" Hi there. What is your name?");
+    System.out.println(" Hi there. What is your name?"+ name);
     
     name= scan.nextLine();
+    System.out.println("What adjective describes you?"+ adjective)
     adjective=scan.nextLine();
 
-    System.out.println("Hi," + name + "Nice to see you.");
+    System.out.println("My name is"+ name "And im" + adjective);
 
        
 
